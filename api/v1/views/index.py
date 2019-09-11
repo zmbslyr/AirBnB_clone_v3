571cd41b517e08477975a45dcc6738952f1c2bf4
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-11 17:55:50 -0400

    [API][py] Updated to return number of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,20 @@ import models
 def status():
     """Returns a JSON status"""
     return jsonify(status="OK")
+
+
+@app_views.route("/stats")
+def num_objects():
+    """Returns the number of objects by type"""
+    clsDict = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"
+    }
+    newDict = {}
+    for k, v in clsDict.items():
+        newDict[v] = models.storage.count(k)
+    return jsonify(newDict)
