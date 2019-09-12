9fad6869e8f59717411e31c906132728a599a969
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-12 14:34:01 -0400

    [API][py] Slight changes including commenting out parts

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Init file to set up flask module"""
 from flask import Blueprint
 
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
+"""import api.v1.views.states
+import api.v1.views.cities
+import api.v1.views.amenities
+import api.v1.views.users
+import api.v1.views.places
+import api.v1.views.places_reviews
+"""
