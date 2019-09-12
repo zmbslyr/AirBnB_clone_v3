5cc0ab34d9f31cfda5901a269e3df069af8b4d4d
Wolf Hopkins <719@holbertonschool.com>
2019-09-12 18:49:45 -0400

    testing city creation

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -10,10 +10,28 @@ import models
                  strict_slashes=False)
 def getStateCities(state_id):
     """return cities in a given state"""
-    Allcity = []
+    cityList = []
     State = models.storage.get('State', state_id)
     if State is None:
         abort(404)
-    else:
-        Allcity.append(city.to_dict() for city in State.cities)
-    return jsonify(Allcity)
+    Allcity = models.storage.all(City)
+    for city in Allcity.values():
+        if city.state_id == state_id:
+            cityList.append(city.to_dict())
+    return jsonify(cityList)
+
+
+@app_views.route('/states/<string:state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def postCity(state_id):
+    """make a city and add it to db"""
+    if models.storage.get("State", state_id) is None:
+        abort(404)
+    if not request.get_json():
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'name' not in request.get_json():
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    makeCity = City(**request.get_json())
+    setattr(makeCity, "state_id", state_id)
+    makeCity.save()
+    return make_response(jsonify(makeCity.to_dict()), 201)
