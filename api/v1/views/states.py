2ace3828f55a20afc76fa5505d42078582b5fa6c
Wolf Hopkins <719@holbertonschool.com>
2019-09-12 15:29:27 -0400

    fixed imports in states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """Module to create a flask route"""
+from models.base_model import BaseModel
+from flask import Flask, abort, jsonify, make_response, request
 from api.v1.views import app_views
-from flask import jsonify
 import models
 
 
