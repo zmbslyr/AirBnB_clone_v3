fafacb1ea4077a4a8e71ff1401acd4731c576782
Wolf Hopkins <719@holbertonschool.com>
2019-09-12 20:48:07 -0400

    ok last typo, this one was stylistic, line 34/36

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -31,9 +31,9 @@ def userPost():
     """Make a new user"""
     if not request.get_json():
         return make_response(jsonify({"error": "Not a JSON"}), 400)
-    if 'email' not in request.get_json():
+    if "email" not in request.get_json():
         return make_response(jsonify({"error": "Missing email"}), 400)
-    if 'password' not in request.get_json():
+    if "password" not in request.get_json():
         return make_response(jsonify({"error": "Missing password"}), 400)
     newUser = User(**request.get_json())
     newUser.save()
