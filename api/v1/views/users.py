c6d531824f2b11b4f27e134f0161eda7b4d2e953
Wolf Hopkins <719@holbertonschool.com>
2019-09-12 20:45:48 -0400

    another typo, lone 66

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -63,6 +63,6 @@ def userUpdate(user_id):
         return make_response(jsonify({"error": "Not a JSON"}), 400)
     for key, value in request.get_json().items():
         if key not in ["id", "created_at", "updated_at", "email"]:
-            setattr(userUpdate, key, value)
+            setattr(update, key, value)
     update.save()
     return jsonify(update.to_dict())
