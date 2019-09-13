19027be003f2372c85fcda844fdafb04faec2be7
Wolf Hopkins <719@holbertonschool.com>
2019-09-12 20:58:35 -0400

    fixed pep8 error

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -78,7 +78,7 @@ def putPlace(place_id):
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     for key, value in request.get_json().items():
         if key not in ["id", "user_id", "city_id", "created_at",
-                        "updated_at"]:
+                       "updated_at"]:
             setattr(update, key, value)
     update.save()
     return jsonify(update.to_dict())
